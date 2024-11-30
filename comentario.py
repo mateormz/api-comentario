de28e4dd96a5c5568543f99af5e919b77ea31700
mateormz <165747472+mateormz@users.noreply.github.com>
2024-11-29 21:57:00 -0500

    Update comentario.py

--- comentario.py
+++ comentario.py
@@ -1,6 +1,7 @@
 import boto3
 import uuid
 import os
+import json
 
 def lambda_handler(event, context):
     # Entrada (json)
@@ -8,6 +9,8 @@ def lambda_handler(event, context):
     tenant_id = event['body']['tenant_id']
     texto = event['body']['texto']
     nombre_tabla = os.environ["TABLE_NAME"]
+    s3_bucket_ingesta = os.environ["S3_BUCKET_INGESTA"]
+    
     # Proceso
     uuidv1 = str(uuid.uuid1())
     comentario = {
@@ -17,9 +20,20 @@ def lambda_handler(event, context):
           'texto': texto
         }
     }
+    
+    # Guardar el comentario en DynamoDB
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
+    
+    # Guardar el comentario en S3
+    s3 = boto3.client('s3')
+    comentario_json = json.dumps(comentario)
+    s3_key = f'comentarios/{uuidv1}.json'  # Usamos el uuid para crear un nombre único para el archivo
+
+    # Subir el archivo JSON al bucket S3
+    s3.put_object(Bucket=s3_bucket_ingesta, Key=s3_key, Body=comentario_json)
+    
     # Salida (json)
     print(comentario)
     return {
